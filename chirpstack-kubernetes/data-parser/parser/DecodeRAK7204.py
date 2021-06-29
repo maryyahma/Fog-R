401790902108e4aa1ba6274607842008f15b0357
maryyahma <73059514+maryyahma@users.noreply.github.com>
2021-06-30 00:38:16 +0200

    Update DecodeRAK7204.py

--- chirpstack-kubernetes/data-parser/parser/DecodeRAK7204.py
+++ chirpstack-kubernetes/data-parser/parser/DecodeRAK7204.py
@@ -25,40 +25,40 @@ def DecodeRAK7204(hexStr):
             payload_dict['humidity'] = str(float(("{:.1f}".format(parseShort(string[4:6], 16) * 0.01 / 2))) * 100) + "%RH" # unit:%RH
             string = string[6:]
         elif flag == 0x0673: # Atmospheric pressure
-            payload_dict['barometer'] = float("{:.2f}".format((parseShort(string[4:8], 16) * 0.1))) + "hPa" # unit:hPa
+            payload_dict['barometer'] = str(float("{:.2f}".format((parseShort(string[4:8], 16) * 0.1)))) + "hPa" # unit:hPa
             string = string[8:]
         elif flag == 0x0267: # Temperature
-            payload_dict['temperature'] = float("{:.2f}".format((parseShort(string[4:8], 16) * 0.1))) + u"\u2103" 
+            payload_dict['temperature'] = str(float("{:.2f}".format((parseShort(string[4:8], 16) * 0.1)))) + u"\u2103" 
             string = string[8:]
         elif flag == 0x0188: # GPS
-            payload_dict['latitude'] = float("{:.4f}".format((parseTriple(string[4:10], 16) * 0.0001))) + u"\u00b0" # unit:Degree
-            payload_dict['longitude'] = float("{:.4f}".format((parseTriple(string[10:16], 16) * 0.0001))) + u"\u00b0" # unit:Degree
-            payload_dict['altitude'] = float("{:.1f}".format((parseTriple(string[16:22], 16) * 0.01))) + "m" # unit:m
+            payload_dict['latitude'] = str(float("{:.4f}".format((parseTriple(string[4:10], 16) * 0.0001)))) + u"\u00b0" # unit:Degree
+            payload_dict['longitude'] = str(float("{:.4f}".format((parseTriple(string[10:16], 16) * 0.0001)))) + u"\u00b0" # unit:Degree
+            payload_dict['altitude'] = str(float("{:.1f}".format((parseTriple(string[16:22], 16) * 0.01)))) + "m" # unit:m
             string = string[22:]
         elif flag == 0x0371: # Triaxial acceleration
-            payload_dict['acceleration_x'] = float("{:.3f}".format((parseShort(string[4:8], 16) * 0.001))) + "g" # unit:g
-            payload_dict['acceleration_y'] = float("{:.3f}".format((parseShort(string[8:12], 16) * 0.001))) + "g" # unit:g
-            payload_dict['acceleration_z'] = float("{:.3f}".format((parseShort(string[12:16], 16) * 0.001))) + "g" # unit:g
+            payload_dict['acceleration_x'] = str(float("{:.3f}".format((parseShort(string[4:8], 16) * 0.001)))) + "g" # unit:g
+            payload_dict['acceleration_y'] = str(float("{:.3f}".format((parseShort(string[8:12], 16) * 0.001)))) + "g" # unit:g
+            payload_dict['acceleration_z'] = str(float("{:.3f}".format((parseShort(string[12:16], 16) * 0.001)))) + "g" # unit:g
             string = string[16:]
         elif flag == 0x0402: # air resistance
-            payload_dict['gasResistance'] = float("{:.2f}".format((parseShort(string[4:8], 16) * 0.01))) + "kiloohm" # unit:kiloohm
+            payload_dict['gasResistance'] = str(float("{:.2f}".format((parseShort(string[4:8], 16) * 0.01)))) + "kiloohm" # unit:kiloohm
             string = string[8:]
         elif flag == 0x0802: # Battery Voltage
             payload_dict['battery'] = str(float("{:.2f}".format((parseShort(string[4:8], 16) * 0.01)))) + "V" # unit:V
             string = string[8:]
         elif flag == 0x0586: # gyroscope
-            payload_dict['gyroscope_x'] = float("{:.2f}".format((parseShort(string[4:8], 16) * 0.01))) + u"\u00b0" + "/s" # unit:Degree/s
-            payload_dict['gyroscope_y'] = float("{:.2f}".format((parseShort(string[8:12], 16) * 0.01))) + u"\u00b0" + "/s" # unit:Degree/s
-            payload_dict['gyroscope_z'] = float("{:.2f}".format((parseShort(string[12:16], 16) * 0.01))) + u"\u00b0" + "/s" # unit:Degree/s
+            payload_dict['gyroscope_x'] = str(float("{:.2f}".format((parseShort(string[4:8], 16) * 0.01)))) + u"\u00b0" + "/s" # unit:Degree/s
+            payload_dict['gyroscope_y'] = str(float("{:.2f}".format((parseShort(string[8:12], 16) * 0.01)))) + u"\u00b0" + "/s" # unit:Degree/s
+            payload_dict['gyroscope_z'] = str(float("{:.2f}".format((parseShort(string[12:16], 16) * 0.01)))) + u"\u00b0" + "/s" # unit:Degree/s
             string = string[16:]
         elif flag == 0x0902: # magnetometer x
-            payload_dict['magnetometer_x'] = float("{:.2f}".format((parseShort(string[4:8], 16) * 0.01))) + "Mu-Tesla" # unit:Mu-Tesla
+            payload_dict['magnetometer_x'] = str(float("{:.2f}".format((parseShort(string[4:8], 16) * 0.01)))) + "Mu-Tesla" # unit:Mu-Tesla
             string = string[8:]
         elif flag == 0x0a02: # magnetometer y
-            payload_dict['magnetometer_y'] = float("{:.2f}".format((parseShort(string[4:8], 16) * 0.01))) + "Mu-Tesla" # unit:Mu-Tesla
+            payload_dict['magnetometer_y'] = str(float("{:.2f}".format((parseShort(string[4:8], 16) * 0.01)))) + "Mu-Tesla" # unit:Mu-Tesla
             string = string[8:]
         elif flag == 0x0b02: # magnetometer z
-            payload_dict['magnetometer_z'] = float("{:.2f}".format((parseShort(str.substring(4, 8), 16) * 0.01))) + "Mu-Tesla" # unit:Mu-Tesla
+            payload_dict['magnetometer_z'] = str(float("{:.2f}".format((parseShort(str.substring(4, 8), 16) * 0.01)))) + "Mu-Tesla" # unit:Mu-Tesla
             string = string[8:]
         else:
             string = string[7:]
